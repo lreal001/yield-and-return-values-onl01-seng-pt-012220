f5d82348e6a0a2cfc35b0c88e5bbeb24eeb5a83d
Lessllie Real <rlessllie@yahoo.com>
2020-01-31 12:22:18 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,14 +2,16 @@ require 'pry'
 array = ["Tim", "Tom", "Jim"]
 def hello(array)
  
-  i = 0 collection = []
+  i = 0 
+  collection = []
   while i < array.length 
-  
+  collection <<
     yield(array[i])
     
     i += 1
     
   end
+  collection
 end
 
 
